92f1f9e6c1e0dd7cb07062833257cbb304fa82ac
Keenan Chiasson <keenanrc@hotmail.com>
2023-07-17 19:01:54 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by user932953 on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
